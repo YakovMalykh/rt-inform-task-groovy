692d38115bf7a8e3b7befed3e43b2839884b26c2
YakovMalykh <to-yakoff@mail.ru>
2023-01-18 14:21:43 +0300

    решение

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,46 @@
+static void main(String[] args) {
+
+    List<Integer> list = List.of(1, 3, 4, 5, 1, 5, 4, 5, 10)
+
+//  def map = listToMapFirst(list);
+//  def map = listToMapSecond(list);
+    def map = listToMapThird(list)
+
+    println(map)
+}
+
+private static Map<Integer, Iterator> listToMapFirst(List<Integer> list) {
+    Map<Integer, Integer> map = new HashMap<>()
+
+    list.forEach(e -> {
+        if (!map.containsKey(e)) map.put(e, 1)
+        else map.put(e, map.get(e) + 1)
+    })
+    map
+}
+
+private static Map<Integer, Iterator> listToMapSecond(List<Integer> list) {
+    Map<Integer, Integer> map = new HashMap<>()
+
+    for (Integer key : list) {
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + 1)
+        }
+        map.putIfAbsent(key, 1)
+    }
+    map
+}
+
+private static Map<Integer, Iterator> listToMapThird(List<Integer> list) {
+    Map<Integer, Integer> map = new HashMap<>()
+
+    for (Integer key : list) {
+        if (!map.containsKey(key)) {
+            map.put(key, 1)
+        } else {
+            Integer val = map.get(key) + 1
+            map.put(key, val)
+        }
+    }
+    map
+}
